a61c24b38515cddb93e0216e665444003d5e9e1b
ZivCarmi <carmi.ziv@gmail.com>
2026-01-15 17:10:38 +0200

    revert thankyou template

--- woocommerce/checkout/thankyou.php
+++ woocommerce/checkout/thankyou.php
@@ -28,7 +28,7 @@ defined( 'ABSPATH' ) || exit;
 		do_action( 'woocommerce_before_thankyou', $order->get_id() );
 		?>
 
-		<?php if ( $order->has_status( 'on-hold' ) ) : ?>
+		<?php if ( $order->has_status( 'failed' ) ) : ?>
 
 			<p class="woocommerce-notice woocommerce-notice--error woocommerce-thankyou-order-failed"><?php esc_html_e( 'Unfortunately your order cannot be processed as the originating bank/merchant has declined your transaction. Please attempt your purchase again.', 'woocommerce' ); ?></p>
 
